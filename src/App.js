9ecfabc6967eb7171d149366ae0de98d0303ea61
GAVARA-PRABHAS-RAM <prabhasram257@gmail.com>
2024-11-14 22:52:33 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import StudentList from './components/StudentList';
+import FacultyList from './components/FacultyList';
+import DataUpload from './components/DataUpload';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path="/student" element={<StudentList />} />
+          <Route path="/faculty" element={<FacultyList />} />
+          <Route path="/upload" element={<DataUpload />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
